bd13951025588aebc6f731ea2ff8b70f7c745a8a
DiiegoRm <diiegorm@hotmail.com>
2018-11-13 21:03:36 -0500

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
-$dbname       = "prueba";
-$dbusuario    = "diiegorm";
-$dbcontrasena = "";
+$dbname       = "localdb";
+$dbusuario    = "azure";
+$dbcontrasena = "6#vWHD_$";
 
 try{
 	$conexion = new PDO("mysql:host=127.0.0.1;dbname=".$dbname, $dbusuario, $dbcontrasena);
